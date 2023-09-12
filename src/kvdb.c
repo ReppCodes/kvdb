16ff48a1fee3b460a62fa97e71aea45da95d4324
ajr <github@andrewrepp.com>
2023-09-12 11:51:57 -0500

    Some progress on structure
    Got first draft of a few of my handler and utility functions ginned up.
    Coming along.

--- src/kvdb.c
+++ src/kvdb.c
@@ -1,35 +1,166 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <semaphore.h>
+#include <fcntl.h>
 #include <strings.h>
 #include <time.h>
 
 #include "kvdb.h"
 
-void lock_db();
-void unlock_db();
-int set_key(char* key, char* value);
-int get_key(char* key);
-int del_key(char* key);
-int ts_key(char* key);
-
-char* get_timestamp()
+// caller must free resulting buffer
+// TODO -- swap this out for an approach that'll give extra 3 decimal points.
+char* get_curr_ts()
 {
-    // TODO -- swap this out for something that'll give extra 3 decimal points.
+    char* buffer = (char*)malloc(TIMESTAMP_LENGTH);
     time_t timer;
-    char buffer[26];
     struct tm* tm_info;
 
     timer = time(NULL);
     tm_info = localtime(&timer);
 
-    strftime(buffer, 26, "%Y-%m-%d %H:%M:%S", tm_info);
-    puts(buffer);
-    printf("\n%s\n", buffer);
-
-    return 0;
+    strftime(buffer, TIMESTAMP_LENGTH, "%Y-%m-%d %H:%M:%S", tm_info);
+    return buffer;
 }
 
-int main(int argc, char *argv[])
+static char* get_db_ts(char* key, char* dbpath)
 {
+    char* buffer = (char*)malloc(TIMESTAMP_LENGTH);
+    record rec;
+    scan_db_record(key, dbpath, &rec);
+    memcpy(buffer, &rec.init_ts, 26);
+    return buffer;
+}
+
+void init_db_record(record *rec)
+{
+    // null initialize all fields of record
+    rec->is_alive = false;
+    strcpy(rec->init_ts, NULL_TIMESTAMP);
+    strcpy(rec->curr_ts, NULL_TIMESTAMP);
+    rec->key_length = 0;
+    memset(rec->key, 0, KEY_MAX_LENGTH);
+    rec->value_length = 0;
+    memset(rec->value, 0, VALUE_MAX_LENGTH);
+}
+
+void scan_db_record(char* key, char* dbpath, record* rec)
+{
+    int err;
+    FILE* db;
+
+    // null initialize db record before looking for actual data
+    // we rely on safe null values for access and for error handling
+    init_db_record(rec);
+
+
+    db = fopen(dbpath, "r");
+    if (db == NULL) {
+        printf("Error writing key %s to database\n", key);
+        err = ERR_FILESYS;
+        goto cleanup;
+    }
+
+    while (true) {
+        
+    }
+
+cleanup:
+    fclose(db);
+}
+
+void lock_db();
+void unlock_db();
+int get_key(char* key, char* dbpath);
+int del_key(char* key, char* dbpath);
+int ts_key(char* key, char* dbpath);
+
+// as a simplifying workaround, timestamps are default to all 0s, and we
+// just check the leading 0 for validity.
+bool is_valid_ts(char* timestamp)
+{
+    if (timestamp[0] == '0') {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+int set_key(char* key, char* value, char* dbpath)
+{
+    int err = ERR_NOERR;
+    bool del_old_record;
+
+    // bounds checks
+    if (strlen(key) > KEY_MAX_LENGTH || strlen(key) == 0) {
+        printf("Key kength must be between 1 and 25, inclusive\n");
+        return ERR_BADKEY;
+    } else if (strlen(value) > VALUE_MAX_LENGTH || strlen(value) == 0) {
+        printf("Value kength must be between 1 and 1000, inclusive\n");
+        return ERR_BADVALUE;
+    }
+  
+    
+    char* init_ts = get_db_ts(key, dbpath);
+    char* curr_ts = get_curr_ts();
+    if (is_valid_ts(init_ts)) {
+        del_old_record = true;
+    } else {
+        free(init_ts);
+        init_ts = curr_ts;
+        del_old_record = false;
+    }
+
+    record* rec = (record*)malloc(sizeof(record));
+    init_db_record(rec);
+    rec->is_alive = true;
+    strcpy(rec->init_ts, init_ts);
+    strcpy(rec->curr_ts, curr_ts);
+    rec->key_length = strlen(key);
+    strcpy(rec->key, key);
+    rec->value_length = strlen(value);
+    strcpy(rec->value, value);
+
+    FILE* db = fopen(dbpath, "a");
+    if (db != NULL) {
+        if (fwrite(rec, sizeof(record), 1, db)) {
+            printf("Error writing key %s to database\n", key);
+            err = ERR_FILESYS;
+            goto cleanup;
+        }
+        if (fclose(db)) {
+            printf("Error closing database\n");
+            err = ERR_FILESYS;
+            goto cleanup;
+        }
+    } else {
+        printf("Error opening database\n");
+        err = ERR_FILESYS;
+        goto cleanup;
+    }
+
+    // do this at the end, data loss is worse than duplicate keys because later
+    // key is always the valid one so we can always recover.
+    if (del_old_record) {
+      del_key(key, dbpath);
+    }
+
+cleanup:
+    free(curr_ts);
+    curr_ts = NULL;
+    if (init_ts) {
+        free(init_ts);
+    }
+    free(rec);
+    return err;
+}
+
+int main(int argc, char* argv[])
+{
+    int err;
+    // for POC, just use single db-wide lock to be safe for multiprocessing
+    sem_t* db_sem; 
+
     if (argc < 3) {
         printf("Requires at least 3 arguments, only %d provided\n", argc);
         return ERR_BADARGS;
@@ -41,26 +172,43 @@ int main(int argc, char *argv[])
     char* cmd = argv[1];
     char* key = argv[2];
     char* value;
-
     if (argc == 4) {
         value = argv[3];
     }
 
+    // keep this separate and passed in for better modularity in future work
+    char* dbpath = "kv.db";
+
+    db_sem = sem_open(dbpath, O_CREAT, SEM_PERMS, 1);
+    if (db_sem == SEM_FAILED) {
+        printf("Unable to acquire lock on database\n");
+        return ERR_SEMAPHORE;
+    }
+
+    // TODO -- add timeout loop here
+    if (sem_wait(db_sem) < 0) {
+        printf("Unable to acquire lock on database\n");
+        return ERR_SEMAPHORE;
+    }
+
+
     // invoke correct handler function
     if (strcasecmp(cmd, "get") == 0) {
-        int err = get_key(key);
-        return err;
+        err = get_key(key, dbpath);
     } else if (strcasecmp(cmd, "set") == 0) {
-        int err = set_key(key, value);
-        return err;
+        err = set_key(key, value, dbpath);
     } else if (strcasecmp(cmd, "del") == 0) {
-        int err = del_key(key);
-        return err;
+        err = del_key(key, dbpath);
     } else if (strcasecmp(cmd, "ts") == 0) {
-        int err = ts_key(key);
-        return err;
+        err = ts_key(key, dbpath);
     } else {
         printf("Unknown command. Accepted commands are: get, set, del, ts.\n");
-        return  ERR_BADCMD; 
+        err = ERR_BADCMD;
     }
+
+    if (sem_post(db_sem) < 0) {
+        printf("Error releasing lock on database\n");
+        err = ERR_SEMAPHORE;
+    }
+    return err;
 }
--- src/kvdb.h
+++ src/kvdb.h
@@ -1,21 +1,40 @@
-#define KEY_MAX_LENGTH 100
-#define VALUE_MAX_LENGTH 10000
+#include <stdbool.h>
+#include <sys/stat.h>
 
-typedef struct entry {
+#define KEY_MAX_LENGTH 25
+#define TIMESTAMP_LENGTH 26
+#define VALUE_MAX_LENGTH 1000
+#define NULL_TIMESTAMP "000-00-00 00:00:00"
+#define SEM_PERMS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP)
+
+typedef struct record {
+    bool is_alive;
+    char init_ts[TIMESTAMP_LENGTH];
+    char curr_ts[TIMESTAMP_LENGTH];
+    int key_length;
     char key[KEY_MAX_LENGTH];
+    int value_length;
     char value[VALUE_MAX_LENGTH];
-} entry;
+} record;
 
+extern bool is_valid_ts(char* timestamp);
+extern char* get_curr_ts();
+extern int del_key(char* key, char* dbpath);
+extern int get_key(char* key, char* dbpath);
+extern int set_key(char* key, char* value, char* dbpath);
+extern int ts_key(char* key, char* dbpath);
 extern void lock_db();
+extern void scan_db_record(char* key, char* dbpath, record* rec);
+extern void init_db_record(record* rec);
 extern void unlock_db();
-extern int set_key(char* key, char* value);
-extern int get_key(char* key);
-extern int del_key(char* key);
-extern int ts_key(char* key);
 
 // error values
 enum {
-    NOERR,
+    ERR_NOERR,
     ERR_BADARGS,
-    ERR_BADCMD
+    ERR_BADCMD,
+    ERR_FILESYS,
+    ERR_BADKEY,
+    ERR_BADVALUE,
+    ERR_SEMAPHORE
 };
